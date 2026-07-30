c3ce9c89a3d429331905346e0fac705c73d00cda
unknown <sahildhole09@gmail.com>
2026-07-30 22:47:56 +0530

    Updated Assignment

--- Assignment34/mail.py
+++ Assignment34/mail.py
@@ -4,8 +4,8 @@ from email.message import EmailMessage
 
 def SendMail(FileName, ReceiverMail):
     try:
-        SenderMail = "sahildhole101@gmail.com"
-        AppPassword = "wlvt ihop qqtm hsdm"
+        SenderMail = "your@gmail.com"
+        AppPassword = "your app password"
 
         msg = EmailMessage()
 
